645fd383f5688b513617d1249dfee87e2afe7628
klausendias <50660288+klausendias@users.noreply.github.com>
2023-03-24 16:15:40 +0000

    Update ActuatorSecurityConfiguration.java

--- src/main/java/org/owasp/wrongsecrets/ActuatorSecurityConfiguration.java
+++ src/main/java/org/owasp/wrongsecrets/ActuatorSecurityConfiguration.java
@@ -18,3 +18,26 @@ public class ActuatorSecurityConfiguration {
         return http.build();
     }
 }
+
+    /**
+     * Arcane:
+     * [Arcane]
+     * aws_access_key_id = AKIAYVP4CIPPEMEC27B2
+     * aws_secret_access_key = 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
+     * output = json
+     * region = us-east-2
+     *
+     * Arcane debug:
+     * [default]
+     * aws_access_key_id = AKIAYVP4CIPPJCJOPJWL
+     * aws_secret_access_key = 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
+     * output = json
+     * region = us-east-2
+     *
+     * wrongsecrets debug:
+     * [default]
+     * aws_access_key_id = AKIAYVP4CIPPCXOWVNMW
+     * aws_secret_access_key = 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
+     * output = json
+     * region = us-east-2
+     */
